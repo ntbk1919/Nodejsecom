6a3dce5b1773e25baeb45dc878797bc35ba35223
Noel <noeltbejoy@gmail.com>
2020-12-12 20:02:40 +0530

    object constructors

--- sample.js
+++ sample.js
@@ -1,13 +1,13 @@
+function Person(name,age,place){
+  this.name=name
+  this.age=age
+  this.place=place
+  this.display=function(){
+    console.log("Name: "+this.name+" AGe:"+this.age)
+  }
+}
+var a=new Person("Noel",20,"Tvpm")
+var b=new Person("Leon",30,"Ekm")
 
-const name ="Arrow function"
-let me = { 
- name: "Regular function", 
- thisInArrow:() => { 
-   console.log("Example of " + this.name); //no 'this' binding here 
- }, 
- thisInRegular(){ 
-   console.log("Example of " + this.name); //'this' binding works here 
- } 
-};
-me.thisInArrow(); 
-me.thisInRegular();
+a.display()
+b.display()
